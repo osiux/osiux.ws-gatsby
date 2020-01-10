06e129e0c5049a98c3b03ae13baaeeebb7191e14
Eduardo Reveles <me@osiux.ws>
2020-01-09 20:45:45 -0600

    new article

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false;
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'react-hint/css/index.css';
@@ -9,3 +8,5 @@ import 'prismjs/plugins/command-line/prism-command-line.css';
 
 import 'typeface-oswald';
 import 'typeface-pt-sans';
+
+config.autoAddCss = false;
