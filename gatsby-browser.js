8d5fbb920a3e4aad567dc014748ebd9c5fdc51e6
Eduardo Reveles <me@osiux.ws>
2020-01-09 20:12:37 -0600

    get rid of huge fa icons on first load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'react-hint/css/index.css';
 import 'prismjs/themes/prism-okaidia.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
