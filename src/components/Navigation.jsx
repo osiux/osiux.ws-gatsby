eed44058529be83281c492c6a25215a956f6a2eb
Eduardo Reveles <me@osiux.ws>
2020-05-01 13:31:45 -0500

    more tq changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,22 +14,11 @@ import {
 import { DarkModeContext } from '../context/DarkModeContext';
 
 const Nav = styled.nav`
-    ${tw`flex flex-row justify-start items-center flex-wrap mb-2`}
-
-    ${(props) => props.theme.breakpoints.desktop} {
-        justify-content: flex-end;
-        align-items: center;
-    }
+    ${tw`flex flex-row justify-start items-center flex-wrap mb-2 md:justify-end md:items-center w-full`}
 `;
 
 const DarkModeButton = styled.button`
-    ${tw`bg-transparent p-0 border-0 order-2 outline-none m-0 mt-4`};
-    color: ${(props) => props.theme.colors.text};
-    transition: color ${(props) => props.theme.transition};
-
-    ${(props) => props.theme.breakpoints.desktop} {
-        order: 3;
-    }
+    ${tw`bg-transparent p-0 border-0 order-2 outline-none m-0 mt-4 text-black transition-colors duration-500 ease-linear md:order-3`};
 `;
 
 const ToggleMenuButton = styled.button`
@@ -51,20 +40,13 @@ const ToggleMenuButton = styled.button`
 `;
 
 const NavListItem = styled.li`
-    ${tw`m-0 p-0 w-full text-center`}
-
-    ${(props) => props.theme.breakpoints.desktop} {
-        width: auto;
-        display: block;
-    }
+    ${tw`m-0 p-0 w-full text-center md:w-auto md:block`}
 `;
 
 const NavLink = styled(Link)`
-    ${tw`inline-block no-underline w-full`}
+    ${tw`inline-block no-underline w-full text-black transition-colors duration-500 ease-linear`}
     padding: 5px 0;
-    color: ${(props) => props.theme.colors.text};
     font-family: 'Oswald', sans-serif;
-    transition: ${(props) => `color ${props.theme.transition}`};
 
     &:hover {
         ${tw`underline`};
@@ -82,25 +64,11 @@ const Navigation = ({ siteTitle }) => {
     const _toggleMenu = () => setMenuOpen((prev) => !prev);
 
     const NavList = styled.ul`
-        list-style: none;
+        ${tw`list-none flex-wrap items-center m-0 p-0 w-full order-3 md:order-2 md:w-auto md:flex md:flex-wrap md:flex-row md:justify-end`}
         display: ${menuOpen ? 'flex' : 'none'};
-        flex-wrap: wrap;
-        align-items: center;
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        order: 3;
-
-        ${(props) => props.theme.breakpoints.desktop} {
-            order: 2;
-            width: auto;
-            display: flex;
-            flex-flow: row wrap;
-            justify-content: flex-end;
-        }
 
         .current {
-            text-decoration: underline;
+            ${tw`underline`};
         }
     `;
 
@@ -109,19 +77,9 @@ const Navigation = ({ siteTitle }) => {
             <Link
                 to="/"
                 css={(theme) => css`
-                    align-self: flex-start;
+                    ${tw`self-start inline-block text-black text-5xl no-underline capitalize order-1 w-auto outline-none flex-grow transition-colors duration-500 ease-linear`}
                     padding: 5px 0 0 10px;
-                    display: inline-block;
-                    color: ${theme.colors.text};
-                    font-size: 30px;
-                    text-decoration: none;
-                    text-transform: capitalize;
                     font-family: 'Oswald', sans-serif;
-                    order: 1;
-                    width: auto;
-                    outline: none;
-                    flex-grow: 1;
-                    transition: color ${theme.transition};
 
                     ${theme.breakpoints.desktop} {
                         padding: 5px 0 0;
