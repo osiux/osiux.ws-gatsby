3815621690afbd21af9633be496bba6976344e8e
Eduardo Reveles <me@osiux.ws>
2020-07-26 18:42:59 -0500

    refactor gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,14 +22,12 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     }
 };
 
-exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions;
-
+const makeBlogPages = async ({ actions, graphql }) => {
     const postTemplate = path.resolve('src/templates/post.jsx');
     const postsListingTemplate = path.resolve('src/templates/postsListing.jsx');
 
-    return graphql(`
-        {
+    const { errors, data } = await graphql(`
+        query getAllPostsData {
             allMarkdownRemark(
                 sort: { order: DESC, fields: frontmatter___date },
                 ${
@@ -38,57 +36,63 @@ exports.createPages = ({ actions, graphql }) => {
                         : ''
                 }
             ) {
-                edges {
-                    node {
-                        frontmatter {
-                            date(formatString: "MMM D, YYYY")
-                            title
-                        }
-                        fields {
-                            slug
-                        }
+                nodes {
+                    frontmatter {
+                        title
+                        date(formatString: "MMM D, YYYY")
+                        category
+                        tags
+                    }
+                    fields {
+                        slug
                     }
                 }
             }
         }
-    `).then((result) => {
-        if (result.errors) {
-            return Promise.reject(result.errors);
-        }
+    `);
 
-        const posts = result.data.allMarkdownRemark.edges;
+    if (errors) {
+        throw new Error(errors);
+    }
 
-        // Pagination
-        const pageCount = Math.ceil(posts.length / POSTS_PER_PAGE);
+    const { createPage } = actions;
 
-        [...Array(pageCount)].forEach((_val, pageNum) => {
-            createPage({
-                path: pageNum === 0 ? `/blog/` : `/blog/page-${pageNum + 1}/`,
-                component: postsListingTemplate,
-                context: {
-                    limit: POSTS_PER_PAGE,
-                    skip: pageNum * POSTS_PER_PAGE,
-                    pageCount,
-                    currentPageNum: pageNum + 1,
-                },
-            });
-        });
+    const posts = data.allMarkdownRemark.nodes;
 
-        // Individual Article
-        posts.forEach((post, index) => {
-            const previous =
-                index === posts.length - 1 ? null : posts[index + 1].node;
-            const next = index === 0 ? null : posts[index - 1].node;
+    // Pagination
+    const pageCount = Math.ceil(posts.length / POSTS_PER_PAGE);
 
-            createPage({
-                path: `blog/${post.node.fields.slug}`,
-                component: postTemplate,
-                context: {
-                    slug: post.node.fields.slug,
-                    previous,
-                    next,
-                },
-            });
+    [...Array(pageCount)].forEach((_val, pageNum) => {
+        createPage({
+            path: pageNum === 0 ? `/blog/` : `/blog/page-${pageNum + 1}/`,
+            component: postsListingTemplate,
+            context: {
+                limit: POSTS_PER_PAGE,
+                skip: pageNum * POSTS_PER_PAGE,
+                pageCount,
+                currentPageNum: pageNum + 1,
+            },
         });
     });
+
+    // Individual Article
+    posts.forEach((post, index) => {
+        const previous =
+            index === posts.length - 1 ? null : posts[index + 1].node;
+        const next = index === 0 ? null : posts[index - 1].node;
+
+        createPage({
+            path: `blog/${post.fields.slug}`,
+            component: postTemplate,
+            context: {
+                slug: post.fields.slug,
+                previous,
+                next,
+            },
+        });
+    });
+};
+
+exports.createPages = async ({ actions, graphql }) => {
+    await Promise.all([makeBlogPages({ actions, graphql })]);
 };
